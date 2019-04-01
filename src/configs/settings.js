31c62f4c07ff8ca566e86ffe7a7ec0878d446789
gadokkwon <gadokkwon@gmail.com>
2019-04-01 16:31:00 +0900

    refac

--- src/configs/settings.js
+++ src/configs/settings.js
@@ -1,7 +1,19 @@
-const appSetting = {
-    'VUE_APP_FIREBASE_KEY': process.env === 'production' ? 'AIzaSyAQSGTB5t2z0_ZU7x3cw4RnmZZHuL3UhaM' : 'AIzaSyBN9yUx2YaxeTZ7jIxQ52lDl0c5Y0yoUHQ',
-    'VUE_APP_GRAPHQL_HTTP': process.env === 'production' ? 'https://api.strix.kr/graphql' : 'https://api.dev.strix.kr/graphql',
-    'VUE_APP_GRAPHQL_WS': process.env === 'production' ? 'wss://api.strix.kr/graphql' : 'wss://api.dev.strix.kr/graphql'
+const namespace = process.env.VUE_APP_NAMESPACE || 'dev'
+const appSettings = {
+    prod: {
+        'VUE_APP_FIREBASE_KEY': 'AIzaSyAQSGTB5t2z0_ZU7x3cw4RnmZZHuL3UhaM',
+        'VUE_APP_GRAPHQL_HTTP': 'https://gw.strix.co.kr/graphql',
+        'VUE_APP_GRAPHQL_WS': 'wss://gw.strix.co.kr/graphql'
+    },
+    dev: {
+        'VUE_APP_FIREBASE_KEY': 'AIzaSyBN9yUx2YaxeTZ7jIxQ52lDl0c5Y0yoUHQ',
+        'VUE_APP_GRAPHQL_HTTP': 'https://gw.dev.strix.co.kr/graphql',
+        'VUE_APP_GRAPHQL_WS': 'wss://gw.dev.strix.co.kr/graphql'
+    }
 }
+const appSetting = appSettings[namespace]
+if (!appSetting) throw new Error('not existing namespace configuration', namespace)
+// TODO: remove this!!!
+console.info({ ...appSetting, ...process.env })
 
 export default appSetting
