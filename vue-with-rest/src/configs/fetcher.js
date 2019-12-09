171169b1788e516ff2015594c6b8a764a2b62ab9
eonnine <fnsldkzbfl@naver.com>
2019-12-09 17:41:36 +0900

    add apiConfigs property in package.json

--- vue-with-rest/src/configs/fetcher.js
+++ vue-with-rest/src/configs/fetcher.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 import packageJson from '../../package.json';
 
-const { endPoints } = packageJson;
+const { apiConfigs } = packageJson;
 
-axios.defaults.baseURL = endPoints.sample;
+const env = process.env.VUE_APP_ENV === 'production' ? 'production' : 'development';
+
+axios.defaults.baseURL = apiConfigs[env].endPoint;
 axios.defaults.headers.common['Content-Type'] = 'application/json; charset=UTF-8';
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
