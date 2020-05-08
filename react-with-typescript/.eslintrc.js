4c57adca34317143555c8adf31c55445581ab3b2
Thomas <gadokkwon@gmail.com>
2020-05-08 14:15:23 +0900

    add eslint rules

--- react-with-typescript/.eslintrc.js
+++ react-with-typescript/.eslintrc.js
@@ -37,6 +37,9 @@ module.exports = {
     'operator-linebreak': 0,
     'no-use-before-define': 0,
     'no-param-reassign': 0,
+    'no-unused-vars': 0,
+    'import/no-unresolved': 0,
+    'import/extensions': 0,
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
 };
