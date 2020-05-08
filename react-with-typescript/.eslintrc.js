2d3df81cde129f88061f980d94b0ab41dc174040
Thomas <gadokkwon@gmail.com>
2020-05-08 13:07:47 +0900

    init typescript

---
+++ react-with-typescript/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    "airbnb-base"
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
+  rules: {
+  },
+};
---
+++ react-with-typescript/config-overrides.js
@@ -0,0 +1,36 @@
+const {
+  override,
+  addWebpackPlugin,
+  disableChunk,
+  addLessLoader,
+  removeModuleScopePlugin,
+  addWebpackAlias,
+  fixBabelImports,
+} = require('customize-cra');
+const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin');
+const { resolve } = require('path');
+const sassToJS = require('sass-vars-to-js');
+
+const themeVariables = sassToJS(
+  resolve(__dirname, 'src/styles/variables.scss'),
+);
+
+// refs = https://github.com/arackaf/customize-cra
+module.exports = override(
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true,
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    importLoaders: true,
+    modifyVars: themeVariables,
+  }),
+  disableChunk(),
+  removeModuleScopePlugin(),
+  addWebpackPlugin(new AntdDayjsWebpackPlugin()),
+  addWebpackAlias({
+    '@': resolve(__dirname, 'src'),
+  }),
+);
